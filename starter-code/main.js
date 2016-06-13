f1c7dd519a5d4d9c2f920ca7d78cba2119893a01
kaylinishime <kmnishime@ucdavis.edu>
2016-06-13 14:42:56 -0700

    conditionals added

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,12 @@ var cardTwo = "queen";
 var cardThree = "king";
 var cardFour = "king";
 
+if (cardOne === cardTwo) {
+	alert("You found a match!");
+}
+else if (cardThree === cardFour) {
+	alert("You found a match!");
+}
+else {
+	alert("Sorry, try again.");
+}
